a2edc39ce07793c2ddf5ba824af85760147debf1
Hrytsenia Nazar <hrytseniaaa@gmail.com>
2022-08-12 20:50:02 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.com/v3.1/name/chile').then(response => {
+    return response.json();
+})
+.then(country => {
+    console.log(country);
+})
+ 
+
