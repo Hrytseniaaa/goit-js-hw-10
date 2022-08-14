ae55aa9ef008db4999637ed48d8f3a69df043d29
Hrytseniaaa <106759647+Hrytseniaaa@users.noreply.github.com>
2022-08-14 22:32:49 +0000

    Deploying to gh-pages from @ Hrytseniaaa/goit-js-hw-10@0259a618eb0dfe7a6b9a5bcd682042a02a94d6cd 🚀

--- index.6a2ad638.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/chile").then((function(n){return n.json()})).then((function(n){console.log(n)}));
-//# sourceMappingURL=index.6a2ad638.js.map
--- index.e61c09da.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/chile").then((e=>e.json())).then((e=>{console.log(e)}));
-//# sourceMappingURL=index.e61c09da.js.map
